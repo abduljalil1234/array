a0dd5130d026e1ab13ec2fad436f9d4f16089664
Abdul Jalil <jolila305@gmail.com>
2022-01-24 18:51:59 +0600

    first commit

---
+++ array/associative.php
@@ -0,0 +1,395 @@
+<?php 
+	
+	$arrr1 = [
+				'name'=>'abdus salam',
+				'age'=>25,
+				'subject'=>'computer'
+	];
+	echo "The result is:".$arrr1['name'].'<br/>';
+
+	$arrra2 = [
+				'a'=>'abdul kader',
+				'b'=>'civil',
+				'c'=>35
+	];
+	echo "The result is:".$arrra2['b'].'<br/>';
+
+	$arrr3 = [
+				'p'=>'password',
+				'c'=>'code',
+				'r'=>'result'
+	];
+	echo "The result is:".$arrr3['r'].'<br/>';
+	$arrr4 = [
+				'p'=>'point',
+				'n'=>'name',
+				'b'=>25
+	];
+	echo "The result is:".$arrr4['b'].'<br/>';
+
+	$arrr5 = [
+				'g'=>'government',
+				'p'=>'points',
+				'r'=>'results'
+	];
+	echo "The result is:".$arrr5['g'].'<br/>';
+
+	$arrr6 = [
+				'h'=>'country',
+				'w'=>'world',
+				'd'=>'panchagarh'
+	];
+	echo "The six result is:".$arrr6['d'].'<br/>';
+
+	$arrr7 = [
+				'r'=>'rangpur',
+				'd'=>'dinajpur',
+				'T'=>'Thakurgaon'
+	];
+	echo "The seven result is:".$arrr7['r'].'<br/>';
+	$arrr8 = [
+				'c'=>'cumilla',
+				't'=>'chittagaong',
+				'b'=>'bandorban'
+	];
+	echo "The eight result is:".$arrr8['t'].'<br/>';
+
+	$arrr9 = [
+				'g'=>'gaibandha',
+				'h'=>'hatibandha',
+				'b'=>'bogora'
+	];
+	echo "The nine result is:".$arrr9['h'].'<br/>';
+
+	$arrr10 = [
+				'n'=>'narayounganj',
+				't'=>'titumir',
+				's'=>'sirazganj'
+	];
+	echo "The Ten result is:".$arrr10['s'].'<br/>';
+
+	$arrr11 = [
+				'p'=>'password',
+				'n'=>35433,
+				'v'=>'value'
+	];
+	echo "The result is:".$arrr11['p'].'<br/>';
+
+	$arrr12 = [
+				'd'=>'debiganj',
+				'u'=>'upazila',
+				'g'=>'darahat'
+	];
+	echo "The result is:".$arrr12['d'].'<br/>';
+	$arrr13 = [
+				'k'=>'kaliganj',
+				's'=>'sonahar',
+				'f'=>'fullbari'
+	];
+	echo "The result is:".$arrr13['k'].'<br/>';
+
+	$arrr14 = [
+				'm'=>'middle',
+				's'=>'simple',
+				'h'=>'high'
+	];
+	echo "The result is:".$arrr14['m'].'<br/>';
+
+	$arrr15 = [
+				'm'=>'madrasa',
+				'c'=>'company',
+				'e'=>'european'
+	];
+
+	echo "The result is:".$arrr15['e'].'<br/>';
+
+	$arrr16 = [
+				'a'=>'arabic',
+				'b'=>'banga',
+				'e'=>'english',
+				'm'=>'math'
+	];
+	echo "The result is:".$arrr16['m'].'<br/>';
+
+	$arrr17 = [
+				'a'=>'alamin',
+				'b'=>'bkash',
+				'r'=>'rocket',
+				'i'=>'islamic'
+	];
+	echo "The result is:".$arrr17['i'].'<br/>';
+
+	$arrr18 = [
+				'r'=>'abdur rahman',
+				'k'=>'abdul khalek',
+				's'=>'abdus salam'
+	];
+	echo "The result is:".$arrr18['r'].'<br/>';
+
+	$arrr19 = [
+				'b'=>'bangladesh',
+				'p'=>'pakisthan',
+				'a'=>'afganisthan'
+	];
+	echo "The result is:".$arrr19['p'].'<br/>';
+
+	$arrr20 = [
+				'a'=>'australia',
+				'i'=>'india',
+				'b'=>'brazil'
+	];
+	echo "The twenty result is:".$arrr20['b'].'<br/>';
+
+	$arrr21 = [
+				'b'=>'bangladesh',
+				'c'=>'cumilla',
+				'd'=>'Dhaka'
+	]; 
+	echo "The result is:".$arrr21['d'].'<br/>';
+
+	$arrr22 = [
+				'a'=>'abdul khalek',
+				's'=>'abdus salam',
+				'j'=>'abdul jalil'
+	];
+	echo "The result is:".$arrr22['j'].'<br/>';
+
+	$arrr23 = [
+				'n'=>'Nougaon',
+				'b'=>'bogora',
+				's'=>'sirazganj'
+	];
+	echo "The result is:".$arrr23['n'].'<br/>';
+
+	$arrr24 = [
+				'l'=>'laravel',
+				'p'=>'php',
+				'j'=>'javascript'
+	];
+	echo "The result is:".$arrr24['l'].'<br/>';
+
+	$arrr25 = [
+				'v'=>'vuejs',
+				'n'=>'nuxtjs',
+				'j'=>'javascript'
+	];
+	echo "The result is:".$arrr25['v'].'<br/>';
+
+	$arrr26 = [
+				's'=>'shantho',
+				'h'=>'hasibul',
+				'a'=>'alamin'
+	];
+	echo "The result is:".$arrr26['h'].'<br/>';
+
+	$arrr27 = [
+				't'=>'table',
+				'c'=>'chaire',
+				'b'=>'bed'
+	];
+	echo "The result is:".$arrr27['t'].'<br/>';
+
+	$arrr28 = [
+				'u'=>'uppercase',
+				'l'=>'lowercase',
+				'c'=>'camelcase'
+	];
+	echo "The result is:".$arrr28['c'].'<br/>';
+
+	$arrr29 = [
+				's'=>'subject',
+				'm'=>'math',
+				'b'=>'bangla'
+	];
+	echo "The result is:".$arrr29['s'].'<br/>';
+
+	$arrr30 = [
+				'a'=>'arabic',
+				'e'=>'english',
+				'b'=>'bangla',
+				'h'=>'hindi'
+
+	];
+	echo "the result is:".$arrr30['a'].'<br/>';
+
+	$arrr31 = [
+				'a'=>'age',
+				'n'=>'name',
+				'y'=>'year'
+	];
+	echo "The result is:".$arrr31['y'].'<br/>';
+
+	$arrr32 = [
+				'sa'=>'samim',
+				'za'=>'zaman',
+				'ra'=>'rasel'
+	];
+	echo "The result is:".$arrr32['ra'].'<br/>';
+
+	$arrr33 = [
+				'p'=>'pias',
+				'm'=>'mula',
+				'a'=>'alu'
+	];
+	echo "The result is:".$arrr33['p'].'<br/>';
+
+	$arrr34 = [
+				's'=>'sahin',
+				'n'=>'nurnobi',
+				'a'=>'asraful',
+				'so'=>'soriful islam'
+	];
+	echo "The result is:".$arrr34['so'].'<br/>';
+
+	$arrr35 = [
+				'd'=>'doctor',
+				'e'=>'engineer',
+				't'=>'teacher'
+	];
+	echo "The result is:".$arrr35['d'].'<br/>';
+
+	$arrr36 = [
+				'p'=>'password',
+				'n'=>35433,
+				'v'=>'value'
+	];
+	echo "The result is:".$arrr36['p'].'<br/>';
+
+	$arrr37 = [
+				'd'=>'dim',
+				'u'=>'upper',
+				'g'=>'got'
+	];
+	echo "The result is:".$arrr37['d'].'<br/>';
+	$arrr38 = [
+				'f'=>'food',
+				's'=>'single',
+				'd'=>'decimal'
+	];
+	echo "The result is:".$arrr38['d'].'<br/>';
+
+	$arrr39 = [
+				'f'=>'football',
+				's'=>'structure',
+				'h'=>'height'
+	];
+	echo "The result is:".$arrr39['f'].'<br/>';
+
+	$arrr40 = [
+				'm'=>'moon',
+				'c'=>'construction',
+				'b'=>'boolian'
+	];
+
+	echo "The result is:".$arrr40['c'].'<br/>';
+
+	$arrr41 = [
+				'a'=>'arabic',
+				'b'=>'break',
+				'e'=>'english',
+				'm'=>'main'
+	];
+	echo "The result is:".$arrr41['e'].'<br/>';
+
+	$arrr42 = [
+				'a'=>'has',
+				'b'=>'bkash',
+				'r'=>'rocket',
+				'i'=>'islamic university'
+	];
+	echo "The result is:".$arrr17['i'].'<br/>';
+
+	$arrr43 = [
+				'r'=>'abdur rahman',
+				'k'=>'abdul khalek',
+				's'=>'abdus salam',
+				'p'=>'personal site'
+	];
+	echo "The result is:".$arrr43['p'].'<br/>';
+
+	$arrr44 = [
+				'v'=>'vote',
+				'p'=>'panda',
+				'a'=>'alo'
+	];
+	echo "The result is:".$arrr44['p'].'<br/>';
+
+	$arrr45 = [
+				'a'=>'asian university',
+				'i'=>'india',
+				'b'=>'brazil university'
+	];
+	echo "The result is:".$arrr45['b'].'<br/>';
+
+	$arrr46 = [
+				'd'=>'debiganj',
+				'u'=>'upazila',
+				'g'=>'darahat',
+				'c'=>'dhaka college'
+	];
+	echo "The result is:".$arrr46['c'].'<br/>';
+	$arrr47 = [
+				'k'=>'kalke',
+				's'=>'seller',
+				'f'=>'father'
+	];
+	echo "The result is:".$arrr47['f'].'<br/>';
+
+	$arrr48 = [
+				'm'=>'modde',
+				's'=>'single',
+				'h'=>'heroin'
+	];
+	echo "The result is:".$arrr48['m'].'<br/>';
+
+	$arrr49 = [
+				'm'=>'madrasa',
+				'c'=>'company',
+				'e'=>'european'
+	];
+
+	echo "The result is:".$arrr49['e'].'<br/>';
+
+	$arrr50 = [
+				'a'=>5634,
+				'b'=>575,
+				'e'=>785,
+				'm'=>23
+	];
+	echo "The result is:".$arrr50['m'].'<br/>';
+
+	$arrr51 = [
+				'a'=>653,
+				'b'=>635,
+				'r'=>663,
+				'i'=>636
+	];
+	echo "The result is:".$arrr51['i'].'<br/>';
+
+	$arrr52 = [
+				'r'=>'rocket',
+				'k'=>'kripon',
+				's'=>'great'
+	];
+	echo "The result is:".$arrr52['r'].'<br/>';
+
+	$arrr53 = [
+				'b'=>'bold',
+				'p'=>'pain',
+				'a'=>'and'
+	];
+	echo "The result is:".$arrr53['b'].'<br/>';
+
+	$arrr54 = [
+				'ba'=>'bangla',
+				'i'=>'islam',
+				'b'=>'bundu'
+	];
+	echo "The result is:".$arrr54['b'].'<br/>';
+
+	$arrr55 = [
+				'p'=>'pulsur',
+				'n'=>'null',
+				'h'=>'hero 100'
+	];
+	echo "The 55 number result is:".$arrr55['p'].'<br/>';
---
+++ array/indexa.php
@@ -0,0 +1,195 @@
+<?php 
+	$arr1 = [12,13,25,345,345,34,63,6,];
+	echo "The first number is:".$arr1[4].'<br/>';
+
+	$arr2 = [34,54,35,34,46,65,634,43,9];
+	echo "The second number is:".$arr2[7].'<br/>';
+
+	$arr3 = [345,463,653,345,35,5,7,89];
+	echo "the third number is:".$arr3[6]."<br/>";
+
+	$arr4 = [34,45,56,46,34,46,4,46,64];
+	echo "the four result is:".$arr4[8].'<br/>';
+
+	$arr5 = [563,345,3453,5,35,3,345,46,63,77,99];
+	echo "The five result is:".$arr5[9].'<br/>';
+
+	$arr6 = [45,32,45,32,4,543,34,56,3];
+	echo "The six result is:".$arr6[4].'<br/>';
+
+	$arr7 = [45,546,35,46,4,5,35,46,446,456];
+	echo "The seven result is:".$arr7[7].'<br/>';
+
+	$arr8 = [456,456,345,3,345,45,2345,34,456,5,3];
+	echo "The eight result is:".$arr8[9].'<br/>';
+
+	$arr9 = [54,456,653,6,57,4,46,4,46,6,4,64,6,65,55];
+	echo "The nine result is:".$arr9[11].'<br/>';
+
+	$arr10 = [36,653,4563,653,636,53,63,563];
+	echo "The ten result is:".$arr10[2].'<br/>';
+
+	$arr11 = ['alamin','sazib','sakib',44,43,'sakib','karim',55];
+	echo 'The eleven result is:'.$arr11[6].'<br/>';
+
+	$arr12 = ['cumilla','dhaka','munsiganj',47,62,'rangpur','dinajpur',55];
+	echo "The result is:".$arr12[1].'<br/>';
+
+	$arr13 = ['hasibul',33,'karim',44,'abdul khalek',54,43,'rahman'];
+	echo "The result is:".$arr13[4].'<br/>';
+
+	$arr14 = ['english','math',64,'banga',66,'arabic'];
+	echo "The result is:".$arr14['5'].'<br/>';
+
+	$arr15 = ['sazzad','samim',32,23,'forid','karim','salam'];
+	echo "The result is:".$arr15[6].'<br/>';
+
+	$arr16 = ['salman','robiul',54,56,'alif','karim','abu darda'];
+	echo "The result is:".$arr16[6].'<br/>';
+
+	$arr17 = ['sazid','abdur rahman',55,57,'sobur'];
+	echo "The result is:".$arr17[2].'<br/>';
+
+	$arr18 = ['hasan',54,'liton','raisul',67,76];
+	echo "The result is:".$arr18[4].'<br/>';
+
+	$arr19 = ['karim','salman','sabbir','selim'];
+	echo "The result is:".$arr19[2].'<br/>';
+
+	$arr20 = ['laravel','PHP','javascript','nodejs','vuejs','reactjs'];
+	echo "The twenty result is:".$arr20[1].'<br/>';
+
+	$arr21 = ['samim','kalam',33,'salam','jakir',32,'alamin',34];
+	echo "The result is:".$arr21[4].'<br/>';
+
+	$arr22 = ['Bangla','English','Arabic',5345,'Hindi',345];
+	echo "The result is:".$arr22[1].'<br/>';
+
+	$arr23 = ['bangladesh',45632,'Pakisthan',345,'INdia','Nepal'];
+	echo "The result is:".$arr23[2].'<br/>';
+
+	$arr24 = ['alauddin',53,'abdul khalek',57,'Belal hossain',44];
+	echo "The result is:".$arr24[4].'<br/>';
+
+	$arr25 = ['samim',46,'helal',46,'karim',44,'salauddin'];
+	echo "The result is:".$arr25[5].'<br/>';
+
+	$arr26 = ['sahabuddin',41,'halim',24,'salam',25];
+	echo "The result is:".$arr26[0].'<br/>';
+
+	$arr27 = ['salauddin',22,'taohid',25,'tanim',65,'tamim'];
+	echo "The result is:".$arr27['2'].'<br/>';
+
+	$arr28 = ['rahim',34,'karim',43,'zubbar'];
+	echo "The result is:".$arr28[3].'<br/>';
+
+	$arr29 = ['abdur rahman',21,'abdul jalil',24,'sahin'];
+	echo "The result is:".$arr29[0].'<br/>';
+
+	$arr30 = ['mahmudul',54,'hasan','nayem',44,'abed ali'];
+	echo "The result is:".$arr30[5].'<br/>';
+
+	$arr31 = ['abid',21,'raisul',20,'sazib',23];
+	echo "The result is:".$arr31[3].'<br/>';
+
+	$arr32 = ['jakir',27,'parvez',29,'samim'];
+	echo "The result is:".$arr32[2].'<br/>';
+
+	$arr33 = ['hasibul',43,'jakir',65,'karim',43,'ebrahim'];
+	echo "The result is:".$arr33[4].'<br/>';
+
+	$arr34 = ['kholil',33,'ebrahim',44,'nurnobi','samim'];
+	echo "The result is:".$arr34[3]."<br/>";
+
+	$arr35 = ['sahin',34,'hasibul',45,'karim',43,'abdul jalil'];
+	echo "The result is:".$arr35[4].'<br/>';
+
+	$arr36 = ['bangladesh',453,'India',45623,'pakisthan'];
+	echo "The 36 number result is:".$arr36[2].'<br/>';
+
+	$arr37 = ['alamin',646,'sazib','sakib',44,43,'sakib','karim',55];
+	echo 'The eleven result is:'.$arr37[3].'<br/>';
+
+	$arr38 = ['cumilla','dhaka',674,5764,'munsiganj',47,62,'rangpur','dinajpur',55];
+	echo "The result is:".$arr38[3].'<br/>';
+
+	$arr39 = ['hasibul',33,'karim',44,44,896,'abdul khalek',54,43,'rahman'];
+	echo "The result is:".$arr39[6].'<br/>';
+
+	$arr40 = ['english','math',64,747,568,'banga',66,'arabic'];
+	echo "The result is:".$arr40[6].'<br/>';
+
+	$arr41 = ['sazzad','samim islam',32,23,'forid','karim','salam'];
+	echo "The result is:".$arr41[2].'<br/>';
+
+	$arr42 = ['salman','abdur rouf',54,56,'alif','karim','abu darda'];
+	echo "The result is:".$arr42[1].'<br/>';
+
+	$arr43 = ['sazid','abdur rahman',6342,45,55,57,'sobur'];
+	echo "The result is:".$arr43[2].'<br/>';
+
+	$arr44 = ['hasan',54,'liton','arif islam','raisul',67,76];
+	echo "The result is:".$arr44[3].'<br/>';
+
+	$arr45 = ['abdul karim','salman','sabbir','selim'];
+	echo "The result is:".$arr45[0].'<br/>';
+
+	$arr46 = ['laravel & laravel','PHP','javascript','nodejs','vuejs','reactjs'];
+	echo "The twenty result is:".$arr46[0].'<br/>';
+
+	$arr47 = ['samim','kalam',33,'abdus salam','jakir',32,'alamin',34];
+	echo "The result is:".$arr47[3].'<br/>';
+
+	$arr48 = ['Bangla','English internation language','Arabic',5345,'Hindi',345];
+	echo "The result is:".$arr48[1].'<br/>';
+
+	$arr49 = ['bangladesh',45632,5634,4567,'Pakisthan',345,'INdia','Nepal'];
+	echo "The result is:".$arr49[2].'<br/>';
+
+	$arr50 = ['alauddin',53,'abdul khalek',57,'Belal hossain',44];
+	echo "The result is:".$arr50[2].'<br/>';
+
+	$arr51 = ['samim',46,'helal',46,'karim',44,'salauddin islam'];
+	echo "The result is:".$arr51[6].'<br/>';
+
+	$arr52 = ['sahabuddin',41,'halim',24,'salam',25];
+	echo "The result is:".$arr52[1].'<br/>';
+
+	$arr53 = ['salauddin',22,'taohid hossain',25,'tanim',65,'tamim'];
+	echo "The result is:".$arr53['2'].'<br/>';
+
+	$arr54 = ['abdur rahim',34,'karim',43,'zubbar'];
+	echo "The result is:".$arr54[0].'<br/>';
+
+	$arr55 = ['abdur rahman',21,'abdul jalil',43,547,24,'sahin'];
+	echo "The result is:".$arr55[2].'<br/>';
+
+	$arr56 = ['got','cow','mahmudul',54,'hasan ali','nayem',44,'abed ali'];
+	echo "The result is:".$arr56[5].'<br/>';
+
+	$arr57 = ['abid islam',21,'raisul',20,'sazib',23];
+	echo "The result is:".$arr57[0].'<br/>';
+
+	$arr58 = ['hm jakir hossain',27,'parvez',29,'samim'];
+	echo "The result is:".$arr58[0].'<br/>';
+
+	$arr59 = ['hasibul',43,'jakir',65346,'kabir vai',43,'ebrahim'];
+	echo "The result is:".$arr59[4].'<br/>';
+
+	$arr60 = ['kholil',33,'ebrahim',44,'nurnobi islam','samim'];
+	echo "The result is:".$arr60[4]."<br/>";
+
+	$arr61 = ['sahin alom',34,'hasibul',45,'karim',43,'abdul jalil'];
+	echo "The result is:".$arr61[0].'<br/>';
+
+	$arr62 = ['bangladesh',453,'India',45623,'pakisthan'];
+	echo "The 62 number result is:".$arr62[4].'<br/>';
+
+	$arr63 = ['great','good','better',45,63432,346,'best'];
+	echo "The result is:".$arr63[2].'<br/>';
+
+	$arr64 = ['result','cow','got','picture',5423,345,4352];
+	echo "The result is:".$arr64[3].'<br/>';
+
+	$arr65 = ['route','current','answer',53,653,'satter'];
+	echo "The 65 number result is:".$arr65[1].'<br/>';
